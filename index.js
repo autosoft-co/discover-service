1c3f1cdc0ae39c85fd44bc230a366ae851e53a08
autosoft-co <47857867+autosoft-co@users.noreply.github.com>
2019-12-06 00:25:31 +0530

    broadcast pcsync service

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var bonjour = require('bonjour')()
+var diont = require('diont')()
 
 // advertise an HTTP server on port 3000
-bonjour.publish({ name: 'My Web Server', type: 'http', port: 3000 })
+bonjour.publish({ name: 'My Web Server', type: '_pcsync._tcp', port: 3000 })
 
 // browse for all http services
-bonjour.find({ type: 'http' }, function (service) {
+bonjour.find({ type: '_pcsync._tcp' }, function (service) {
   console.log('Found an HTTP server:', service)
 })
